83cb71becaa054b9ff15d22e225d6340e769b202
jzilg <jzg@dasburo.com>
2021-01-13 18:23:19 +0100

    add auth for mongodb connection

--- app/db/createClient.ts
+++ app/db/createClient.ts
@@ -1,6 +1,18 @@
 import { MongoClient } from 'mongodb'
 
-const { DB_CLIENT } = process.env
-const createClient = (): MongoClient => new MongoClient(`${DB_CLIENT}`)
+const {
+    DB_CLIENT,
+    DB_AUTH_SOURCE,
+    DB_AUTH_USER,
+    DB_AUTH_PASSWORD,
+} = process.env
+
+const createClient = (): MongoClient => new MongoClient(DB_CLIENT as string, {
+    authSource: DB_AUTH_SOURCE as string,
+    auth: {
+        user: DB_AUTH_USER as string,
+        password: DB_AUTH_PASSWORD as string,
+    },
+})
 
 export default createClient
